dbddaf5724c796c1061f96ab81ae3694c7a736e6
mvbrahmam33 <timetotraveltotime@gmail.com>
2024-10-29 22:17:25 +0530

    DetectionCompletedNeededOptimization

--- app/src/main/java/com/brahmam33/facedetectionandrecognitionandroid/MainActivity.java
+++ app/src/main/java/com/brahmam33/facedetectionandrecognitionandroid/MainActivity.java
@@ -1,36 +1,38 @@
 package com.brahmam33.facedetectionandrecognitionandroid;
 
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-
-import org.opencv.android.CameraActivity;
-import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
-import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Mat;
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
-
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.WindowManager;
 import android.widget.Toast;
 
+import org.opencv.android.CameraActivity;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends CameraActivity implements CvCameraViewListener2 {
-    private static final String TAG = "OCVSample::Activity";
+    private static final String TAG = "Main:Activity";
 
     private CameraBridgeViewBase mOpenCvCameraView;
+    private CascadeClassifier faceDetector;
+    private int absoluteFaceSize,x=0;
 
     public MainActivity() {
         Log.i(TAG, "Instantiated new " + this.getClass());
@@ -43,6 +45,32 @@ public class MainActivity extends CameraActivity implements CvCameraViewListener
 
         if (OpenCVLoader.initLocal()) {
             Log.i(TAG, "OpenCV loaded successfully");
+            try {
+                InputStream is = getResources().openRawResource(R.raw.haarcascade_frontalface_default);
+                File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+                File mCascadeFile = new File(cascadeDir, "haarcascade_frontalface_default.xml");
+                FileOutputStream os = new FileOutputStream(mCascadeFile);
+
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = is.read(buffer)) != -1) {
+                    os.write(buffer, 0, bytesRead);
+                }
+                is.close();
+                os.close();
+
+                faceDetector = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+                if (faceDetector.empty()) {
+                    Log.e(TAG, "Failed to load cascade classifier");
+                    faceDetector = null;
+                } else {
+                    Log.i(TAG, "Loaded cascade classifier from " + mCascadeFile.getAbsolutePath());
+                }
+                cascadeDir.delete();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e(TAG, "Failed to load cascade. Exception thrown: " + e);
+            }
         } else {
             Log.e(TAG, "OpenCV initialization failed!");
             (Toast.makeText(this, "OpenCV initialization failed!", Toast.LENGTH_LONG)).show();
@@ -58,12 +86,14 @@ public class MainActivity extends CameraActivity implements CvCameraViewListener
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
 
         mOpenCvCameraView.setCvCameraViewListener(this);
+
     }
 
     @Override
     public void onPause()
     {
         super.onPause();
+        Log.i(TAG, "onPause");
         if (mOpenCvCameraView != null)
             mOpenCvCameraView.disableView();
     }
@@ -72,32 +102,54 @@ public class MainActivity extends CameraActivity implements CvCameraViewListener
     public void onResume()
     {
         super.onResume();
+        Log.i(TAG, "onResume");
         if (mOpenCvCameraView != null)
             mOpenCvCameraView.enableView();
     }
 
     @Override
     protected List<? extends CameraBridgeViewBase> getCameraViewList() {
+        Log.i(TAG, "getcameraViewList");
         return Collections.singletonList(mOpenCvCameraView);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.i(TAG, "onDestroy");
         if (mOpenCvCameraView != null)
             mOpenCvCameraView.disableView();
     }
 
     @Override
     public void onCameraViewStarted(int width, int height) {
+        Log.i(TAG, "camera started");
     }
 
     @Override
     public void onCameraViewStopped() {
+        Log.i(TAG, "camera stopped");
     }
 
     @Override
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-        return inputFrame.rgba();
+        Log.i(TAG, "camera frame");
+        Mat rgba = inputFrame.rgba();
+        Mat gray = inputFrame.gray();
+
+        if (faceDetector != null) {
+            MatOfRect faces = new MatOfRect();
+            faceDetector.detectMultiScale(gray, faces, 1.1, 2, 2,
+                    new Size(absoluteFaceSize, absoluteFaceSize), new Size());
+
+            Rect[] facesArray = faces.toArray();
+            for (Rect face : facesArray) {
+                Imgproc.rectangle(rgba, face.tl(), face.br(), new Scalar(0, 255, 0, 255), 3);
+            }
+        }
+
+        return rgba;
+
+      //  return inputFrame.rgba();
     }
 }
